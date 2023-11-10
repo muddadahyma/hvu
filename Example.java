00d5c68a84e4769ac6c1ac437415fddd81d7959e
Hymavathi <muddada.hv@gmail.com>
2023-11-10 15:38:47 +0530

    my code

---
+++ Example.java
@@ -0,0 +1,20 @@
+class Example
+{
+static int method1(int age)
+{
+return age+3;
+}
+Static void method2( )
+{
+System.out.print("hyma")
+}
+public static void main(String[] s)
+{
+System.out.print(method1(19));
+}
+}
+
+
+
+
+
